0651b7d9570b56cbf05675b61f69d5ec401c024a
Parth Patel <p.patel81@yahoo.com>
2019-12-28 19:54:49 +1300

    Another initial commit with node_modules excluded

---
+++ routes/contacts.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+//@route GET api/contact
+//@desc Get all users contacts
+//@access Private
+router.get('/', (req, res) => {
+  res.send('Get all contacts');
+});
+
+//@route POST api/contact
+//@desc Add a new contact
+//@access Private
+router.post('/', (req, res) => {
+  res.send('Add Contact');
+});
+
+//@route PUT api/contact/:id
+//@desc Update Contact
+//@access Private
+router.put('/:id', (req, res) => {
+  res.send('Update Contact');
+});
+
+//@route DELETE api/contact/:id
+//@desc Delete contact
+//@access Private
+router.delete('/:id', (req, res) => {
+  res.send('Delete Contact');
+});
+
+module.exports = router;
