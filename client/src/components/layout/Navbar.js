647b3976a597f9850da950d34fbf0b102887a7fc
Parth Patel <p.patel81@yahoo.com>
2020-01-04 12:20:40 +1300

    Login and Register components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,6 +17,12 @@ export const Navbar = ({ title, icon }) => {
         <li>
           <Link to='/About'>About</Link>
         </li>
+        <li>
+          <Link to='/Register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/Login'>Login</Link>
+        </li>
       </ul>
     </div>
   );
